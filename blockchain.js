be0f1f54d1d26f06d797d704938e29b809420b5f
Ramiro Cufre <ramirocufre@hotmail.com>
2022-02-23 23:12:51 -0300

    blockchain

---
+++ blockchain.js
@@ -0,0 +1,102 @@
+const CryptoJS = require('crypto-js');
+
+// Crea instancias de Blockchain
+function Blockchain() {
+  this.chain = [];
+  this.pendingTransactions = []; // Transacciones pendientes. Se validan cuando se crea un nuevo bloque al minarse
+  this.createNewBlock(100, '0', '0'); // GENESIS BLOCK: parametros arbitrarios
+}
+
+// Crea un bloque nuevo
+// Las funciones y los atributos dentro de prototype son comunes a todas las instancias
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransactions,
+    nonce, // valor que cambio para llegar al hash requerido (ej '0000xxxxxx') arranca en 0 y va aumentando
+    hash,
+    previousBlockHash,
+  };
+
+  this.pendingTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
+
+// Devuelve el ultimo bloque
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
+// Crea una nueva transaccion
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount,
+    sender,
+    recipient,
+  };
+
+  this.pendingTransactions.push(newTransaction);
+
+  return this.getLastBlock()['index'] + 1; //devuelve el indice del bloque al que la transaccion se añadira
+};
+
+// Crea el hash mediante SHA-256
+Blockchain.prototype.hashBlock = function (
+  prevBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString =
+    prevBlockHash + // string
+    nonce.toString() + // nro
+    JSON.stringify(currentBlockData); // obj
+
+  // Genera el hash sha256 y lo transforma a hexadecimal
+  const hash = CryptoJS.enc.Hex.stringify(
+    CryptoJS.SHA256(dataAsString)
+  );
+
+  return hash;
+};
+
+// Encuentra el valor de nonce que satisface la dificultad
+Blockchain.prototype.proofOfWork = function (
+  prevBlockHash,
+  currentBlockData,
+  dificulty
+) {
+  let nonce = 0;
+  let hash = this.hashBlock(prevBlockHash, currentBlockData, nonce);
+
+  while (hash.substring(0, dificulty) !== '0'.repeat(dificulty)) {
+    nonce = nonce + 1;
+    hash = this.hashBlock(prevBlockHash, currentBlockData, nonce);
+    // console.log(nonce, hash);
+  }
+  console.log('ENCONTRADO');
+  console.log(nonce, hash);
+  return nonce;
+};
+
+module.exports = Blockchain;
+
+/* --------------------------------- CLASES --------------------------------- */
+
+// class Blockchain {
+//   constructor() {
+//     this.chain = [];
+
+//     this.newTransaction = [];
+//   }
+// }
---
+++ test.js
@@ -0,0 +1,41 @@
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+
+console.log(bitcoin);
+
+// const prevBlockHash = 'prev624f85aa3dcc145a665e245fba02bdd1';
+// const currentBlockData = [
+//   { amount: 10, sender: 'aaaa', recipient: 'bbbb' },
+//   { amount: 30, sender: 'cccc', recipient: 'dddd' },
+//   { amount: 1000, sender: 'eeee', recipient: 'ffff' },
+// ];
+
+// bitcoin.proofOfWork(prevBlockHash, currentBlockData, 6);
+
+// console.log(
+//   bitcoin.hashBlock(prevBlockHash, currentBlockData, nonce)
+// );
+
+// bitcoin.createNewBlock(
+//   892348,
+//   'prev624f85aa3dcc145a665e245fba02bdd1',
+//   'hashf1ed604072fc6b778199bbebf44c3e6'
+// );
+
+// bitcoin.createNewBlock(
+//   1118,
+//   'prev624f85aa3dcc145a665e245',
+//   'hashf1ed604072fc6b778199bbebf'
+// );
+
+// bitcoin.createNewTransaction(100, 'ALEX78199bbebf', 'JEN78199bbebf');
+// bitcoin.createNewTransaction(400, 'JANE78199bbebf', 'JEN78199bbebf');
+// bitcoin.createNewTransaction(500, 'BILL78199bbebf', 'JEN78199bbebf');
+
+// bitcoin.createNewBlock(
+//   666754,
+//   'prev624f85aa3dcc145a665e245fba0',
+//   'hashf1ed604072fc6b778199bbebf4'
+// );
+
+// console.log(bitcoin);
